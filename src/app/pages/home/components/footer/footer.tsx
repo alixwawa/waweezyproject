1b4050ab0011c3c9c648f0ac09d48c9ddc4f8234
Alix Wawa <alixwawa@yahoo.com>
2022-12-03 01:22:32 -0600

    icons in footer

--- src/app/pages/home/components/footer/footer.tsx
+++ src/app/pages/home/components/footer/footer.tsx
@@ -10,7 +10,14 @@ export const Footer = () => {
                         <h1 className='builtby'>{'This Website Was Built By Alix Wawa'}</h1>
                     </tr>
                     <tr className='footer-row-two'>
-                        <h1 className='builtby'>{'This Website Was Built By Alix Wawa'}</h1>
+                        <a href="https://www.facebook.com/therealwaweezy/" className="fa fa-facebook">{ }</a>
+                        <a href="https://twitter.com/TheRealWaweezy" className="fa fa-twitter">{ }</a>
+                        <a href="https://www.reddit.com/user/Waweezy" className="fa fa-reddit">{}</a>
+                        <a href="https://www.youtube.com/channel/UCojLZuHh0KobvUa4UD6azjA" className="fa fa-youtube">{}</a>
+                        <a href="https://www.instagram.com/therealwaweezy/?hl=en" className="fa fa-instagram">{}</a>
+                        {/* <a href="https://www.tiktok.com/@therealwaweezy" className="fa fa-tiktok">{}</a>
+                        <a href="https://discord.com/invite/xrMc9kqYDH" className="fa fa-discord">{}</a> */}
+                        <a href="https://soundcloud.com/therealwaweezy" className="fa fa-soundcloud">{}</a>
                     </tr>
                 </table>
             </div>
